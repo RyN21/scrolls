1f762dfbe7010d4b52e4e1de1b3d7317e7875415
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-04-19 17:56:24 -0600

    Add user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "callbacks" do
+    it "creates an auth token before saving if one does not exist" do
+      # See: Devise initializer `config.http_authenticatable = true`
+      binding.pry
+      user = User.new(attributes_for(:user))
+      expect(user.authentication_token).to be_blank
+
+      user.save
+
+      expect(user.reload.authentication_token).to_not be_blank
+    end
+  end
 end
